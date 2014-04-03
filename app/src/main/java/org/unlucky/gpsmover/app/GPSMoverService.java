92674f2f7270d15b843821fbf3cfbaa3af139d7c
Unlucky <unlucky1990@gmail.com>
2014-04-03 21:01:30 +0800

    implement basic mock location function and improve notification

--- app/src/main/java/org/unlucky/gpsmover/app/GPSMoverService.java
+++ app/src/main/java/org/unlucky/gpsmover/app/GPSMoverService.java
@@ -11,10 +11,15 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -23,12 +28,14 @@ import org.unlucky.gpsmover.app.util.Common;
 
 public class GPSMoverService extends Service
         implements SensorEventListener {
-    private static final int UPDATE_INTERVAL_TIME = 5000;
-    private static final int NOTIFICATION_ID = 0x0;
-    private static final float GRAVITY = 9.8f;
+    private static final int UPDATE_INTERVAL_TIME = 1000;
+    private static final int NOTIFICATION_ID = 0x1234;
+    private static final float MAX_GRAVITY = 9.8f / 2;
+    private static final float MIN_GRAVITY = -9.8f / 2;
+    private static final double UPDATE_STEP = 0.000001;
 
+    private double current_lat, current_lng;
     private SensorManager mSensorManager;
-    private LatLng curPos, prevPos;
     private LocationManager mLocationManager;
     private NotificationManager mNotificationManager;
 
@@ -43,9 +50,12 @@ public class GPSMoverService extends Service
         instance = this;
         mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
         mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(NOTIFICATION_ID, createNotification());
+        mLocationManager.addTestProvider(LocationManager.GPS_PROVIDER, false, false, false, false,
+                false, false, false, Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
+        // test part
+        this.current_lng = 120.0;
+        this.current_lat = 30.0;
 
         Common.log("service created!");
     }
@@ -54,7 +64,7 @@ public class GPSMoverService extends Service
     public void onDestroy() {
         instance = null;
         mSensorManager.unregisterListener(this);
-        mNotificationManager.cancel(NOTIFICATION_ID);
+        stopForeground(true);
         Common.log("service destroyed!");
         super.onDestroy();
     }
@@ -63,7 +73,8 @@ public class GPSMoverService extends Service
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_UI);
+        startForeground(NOTIFICATION_ID, createNotification(createLocation()));
         handler.post(updateGpsThread);
         Common.log("service started!");
         return Service.START_STICKY;
@@ -79,13 +90,13 @@ public class GPSMoverService extends Service
      * Create customized notification
      * @return customized notification
      */
-    private Notification createNotification() {
+    private Notification createNotification(Location location) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setContentTitle(getResources().getString(R.string.app_name))
                 .setContentText(String.format(getResources()
-                        .getString(R.string.msg_fake_gps), 120.123456, 30.123456))
+                        .getString(R.string.msg_fake_gps), location.getLongitude(), location.getLatitude()))
                 .setAutoCancel(false);
 
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -98,18 +109,31 @@ public class GPSMoverService extends Service
         return notification;
     }
 
+    private Location createLocation() {
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        location.setLatitude(current_lat);
+        location.setLongitude(current_lng);
+        //location.setAltitude(0.0);
+        location.setAccuracy(1.0f);
+        if (Build.VERSION.SDK_INT >= 17) {
+            location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        }
+        location.setTime(System.currentTimeMillis());
+
+        return location;
+    }
+
     Handler handler = new Handler();
 
     Runnable updateGpsThread = new Runnable() {
         @Override
         public void run() {
-            //TODO update location
-            //Location location = new Location("GPSFaker");
-            //location.setLatitude(curPos.latitude);
-            //location.setLongitude(curPos.longitude);
-            //location.setAltitude(0.0);
-            //location.setTime(System.currentTimeMillis());
-            //mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, location);
+            Location location = createLocation();
+            mLocationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+            mLocationManager.setTestProviderStatus(LocationManager.GPS_PROVIDER, LocationProvider.AVAILABLE,
+                    null, System.currentTimeMillis());
+            mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, location);// update location
+            startForeground(NOTIFICATION_ID, createNotification(location));// update notification
             handler.postDelayed(updateGpsThread, UPDATE_INTERVAL_TIME);
         }
     };
@@ -120,7 +144,7 @@ public class GPSMoverService extends Service
          * @return current location
          */
         public LatLng getLocation() {
-            return curPos;
+            return new LatLng(current_lat, current_lng);
         }
     }
 
@@ -132,11 +156,28 @@ public class GPSMoverService extends Service
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
-            prevPos = curPos;
             float x = event.values[0];
+            x = Math.min(MAX_GRAVITY, x);
+            x = Math.max(MIN_GRAVITY, x);
+            // change longitude, move toward west when x > 0.0, move toward east when x < 0.0
+            current_lng -= UPDATE_STEP * x;
+            // when longitude overflow
+            if (current_lng > 180.0) {
+                current_lng = current_lng - 360.0;
+            } else if (current_lng < -180.0) {
+                current_lng = 360.0 - current_lng;
+            }
+
             float y = event.values[1];
-            float z = event.values[2];
-            Common.log("X->" + x + " Y->" + y + " Z->" + z);
+            y = Math.min(MAX_GRAVITY, y);
+            y = Math.max(MIN_GRAVITY, y);
+            current_lat -= UPDATE_STEP * y;
+            // when latitude overflow
+            if (current_lat > 90.0) {
+                current_lat = 90.0;
+            } else if (current_lat < -90.0) {
+                current_lat = -90.0;
+            }
         }
     }
 }
--- app/src/main/java/org/unlucky/gpsmover/app/MainActivity.java
+++ app/src/main/java/org/unlucky/gpsmover/app/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends FragmentActivity
         initUI();
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
